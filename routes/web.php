d71d58d6cbbe4ee7bb219e25a629e67555991add
omar-a-eid <omerx9988@gmail.com>
2024-11-11 15:57:25 +0200

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index']);
+Route::resource('posts', PostController::class);
+Route::resource('comments', CommentController::class);
